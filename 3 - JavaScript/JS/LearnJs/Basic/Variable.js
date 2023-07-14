2ce7bd7854e392bc4247c1ac7cf6ea58fafd5037
watcharapol Yotadee <watcharapolyotadee@watcharapols-MacBook-Pro.local>
2023-07-14 13:12:31 +0700

    add file learn

---
+++ 3 - JavaScript/JS/LearnJs/Basic/Variable.js
@@ -0,0 +1,17 @@
+// let เป็นการประกาศตัวแปล แบบไม่ระบุบ Type
+// const เป็นการประกาศตัวแปลแบบค่าคงที่
+// ตัวอย่าง
+let msg = "Hello Codecamp Thailand";
+const NAME = "Watcharapol";
+// คำสั่ง alert เอาไว้แสดงผล 
+// กด f12 เพื่อดู
+// ตัวอย่าง
+alert(msg);
+console.log(msg);
+// LAB
+let human, fllName;
+fllName = 'watcharapol';
+human = fllName;
+console.log(fllName);
+// ประกาศแบบ หลายตัวพร้อมกัน
+let myWallet, momName, dadName, addres, age;
